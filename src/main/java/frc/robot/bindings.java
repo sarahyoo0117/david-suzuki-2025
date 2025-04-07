651ab526247854485ee8d45b118435ff860ac827
Gaeun Yoo <gaeunyoo3626@gmail.com>
2025-04-07 10:42:11 -0400

    joystick suppliers

--- src/main/java/frc/robot/bindings.java
+++ src/main/java/frc/robot/bindings.java
@@ -1,9 +1,14 @@
 package frc.robot;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.commands.commands;
 
 public final class bindings {
   public void configureBindings(robot robot) {
-    robot.swerve.setDefaultCommand(commands.teleop_drive(robot.swerve, oi.get_left_stick(), -oi.drive.getRightX()));
+    Supplier<Translation2d> drive_func = () -> oi.get_left_stick();
+    Supplier<Double> turn_func = () -> -oi.drive.getRightX();
+    robot.swerve.setDefaultCommand(commands.teleop_drive(robot.swerve, drive_func, turn_func));
   }
 }
--- src/main/java/frc/robot/commands/commands.java
+++ src/main/java/frc/robot/commands/commands.java
@@ -1,19 +1,22 @@
 package frc.robot.commands;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve;
 
 public class commands {
-    public static final Command teleop_drive(swerve swerve, Translation2d inputs, double omega_input) {
+    public static final Command teleop_drive(swerve swerve, Supplier<Translation2d> drive_func, Supplier<Double> turn_func) {
         final double drive_factor = 3;
         final double steer_factor = 3;
         return swerve.set_speeds(() -> {
+            Translation2d left = drive_func.get();
             return ChassisSpeeds.fromFieldRelativeSpeeds(new ChassisSpeeds(
-                inputs.getX() * drive_factor, 
-                inputs.getY() * drive_factor, 
-                omega_input * steer_factor
+                left.getX() * drive_factor, 
+                left.getY() * drive_factor, 
+                turn_func.get() * steer_factor
             ), swerve.get_heading());
         }); 
     }
